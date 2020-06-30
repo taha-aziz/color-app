57164f7c81feebe84d2afbca486da5859486b03a
Taha Aziz <25tahaaziz25@gmail.com>
2020-06-29 17:12:05 -0700

    add reducers, actions

---
+++ src/actions/changeColors.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export function changeColors(colors){
+    return {
+        type: "CHANGE_COLORS",
+        colors: colors
+    }
+}
+
+export function loadColors(){
+    return(dispatch) => {
+        return axios.get("http://www.colr.org/json/colors/random/7").then(
+            (response) => {
+                updated_colors = response.data.matching_colors.map(c => "#" + c);
+                dispatch(changeColors(updated_colors));
+            }
+        )
+    }
+}
---
+++ src/reducers/main.js
@@ -0,0 +1,19 @@
+import { bindActionCreators } from "redux"
+
+let defaultState = {
+    colors: ["red", "orange", "yellow", "green", "blue", "purple", "violet"]
+}
+
+ const mainReducer = (state=defaultState, action) => {
+     switch(action.type) {
+         case "CHANGE_COLORS" :
+             return {
+                 ...state,
+                 colors: action.colors
+             }
+         default : 
+             return state
+     }
+}
+
+export default mainReducer;
