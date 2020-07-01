a2df77ffcb573e6ab784e90aa29cfb9ba637073d
Taha Aziz <25tahaaziz25@gmail.com>
2020-06-30 20:12:17 -0700

    fix api, browser cache

--- src/components/interior-box.js
+++ src/components/interior-box.js
@@ -9,7 +9,6 @@ class InteriorBox extends React.Component {
                 <div className = "box-2" style={{backgroundColor: `${this.props.colors[1]}`}}></div>
                 <button style={{backgroundColor: `${this.props.colors[2]}`}}
                 onClick={() => {this.props.handleClick()}}> Color Me!</button>
-                <button onClick={() => this.props.apiClick()}>click for api get request</button>
                 <div className="box-3" style={{backgroundColor: `${this.props.colors[3]}`}}></div>
                 <div className="box-4" style={{backgroundColor: `${this.props.colors[4]}`}}></div>
             </div>
